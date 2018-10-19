ea7fbd29958741402927860672327982e8c08c3b
goni <geonhee.oh@cern.ch>
2018-10-19 11:20:46 +0200

    bkup

---
+++ Fitting/AllFree/changeFileName.sh
@@ -0,0 +1,21 @@
+../AllFree/AllParmFree_fitresults_upsilon_PA_DATA_pt0.0-30.0_y-1.93-1.93_muPt4.0.root
+
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt0.0-1.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt0.0-1.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt0.0-30.0_y-1.93-1.93_muPt4.0.root  AllParmFree_fitresults_upsilon_PA_DATA_pt0.0-30.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt10.0-12.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt10.0-12.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt1.0-2.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt1.0-2.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt12.0-14.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt12.0-14.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt12.0-30.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt12.0-30.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt14.0-16.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt14.0-16.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt16.0-18.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt16.0-18.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt18.0-20.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt18.0-20.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt20.0-24.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt20.0-24.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt20.0-30.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt20.0-30.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt2.0-3.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt2.0-3.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt24.0-30.0_y-1.93-1.93_muPt4.0.root AllParmFree_fitresults_upsilon_PA_DATA_pt24.0-30.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt3.0-4.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt3.0-4.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt4.0-5.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt4.0-5.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt5.0-6.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt5.0-6.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt6.0-8.0_y-1.93-1.93_muPt4.0.root   AllParmFree_fitresults_upsilon_PA_DATA_pt6.0-8.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt8.0-10.0_y-1.93-1.93_muPt4.0.root  AllParmFree_fitresults_upsilon_PA_DATA_pt8.0-10.0_y-1.93-1.93_muPt4.0.root
+mv AllParmFree_fitresults_upsilon_DoubleCB_5TeV_PA_DATA_pt9.0-10.0_y-1.93-1.93_muPt4.0.root  AllParmFree_fitresults_upsilon_PA_DATA_pt9.0-10.0_y-1.93-1.93_muPt4.0.root
